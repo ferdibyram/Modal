04e13deab1b921b448bed4883453113025681d09
ferdi bayram <ferdigit@gmail.com>
2025-12-17 03:34:30 +0300

    completed

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const closeModals = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModals = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModals);
+}
+
+btnCloseModal.addEventListener('click', closeModals);
+overlay.addEventListener('click', closeModals);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModals();
+  }
+});
